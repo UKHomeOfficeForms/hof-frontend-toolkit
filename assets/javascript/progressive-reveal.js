150c8fde462aff4fc73077f9423493e0d5b0ca3d
alexfranklin <alex.franklin@digital.homeoffice.gov.uk>
2020-10-13 18:35:10 +0100

    fix: reduce dom tree searches when focussing text field after progressive reveal

--- assets/javascript/progressive-reveal.js
+++ assets/javascript/progressive-reveal.js
@@ -20,10 +20,10 @@ function inputClicked(e, target) {
                 toggle.setAttribute('aria-hidden', 'false');
                 helpers.removeClass(toggle, hiddenClass);
                 shown = toggle.id;
-                if ((input === target) && (toggle.getElementsByTagName('input').length !== 0)) {
-                    toggle.getElementsByTagName('input')[0].focus();
+                var elementToFocus = toggle.getElementsByTagName('input')[0];
+                if ((input === target) && elementToFocus) {
+                    elementToFocus.focus();
                 }
-                
             } else {
                 input.setAttribute('aria-expanded', 'false');
                 if (shown !== toggle.id) {
